6c0475a757d27dbf13d1790c04dc08d29a28f206
mariano <mariano@benitez.nu>
2022-01-18 17:36:57 -0300

    - invocamos al openapi generator #3 - fix del api - instalamos con pnpm

--- bin/build-packages.sh
+++ bin/build-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # exit on first failed command
-#set -x # mostrar cada comando que se ejecuta
+set -x # mostrar cada comando que se ejecuta
 # este script arma la distribución web para todas las variaciones de la app
 #
 # depende de estas variables de entorno
@@ -9,20 +9,24 @@ set -e # exit on first failed command
 # $APP_CODE - el nombre de la aplicacion a compilar
 #
 # transformamos GITHUB_REF_NAME en FCI_BRANCH para unificar criterios
-FCI_BRANCH=${GITHUB_REF_NAME:?No está definida la variable de entorno GITHUB_REF_NAME. No podemos seguir}
+FCI_BRANCH=$(git branch --show-current)
 export FCI_BRANCH
 #
 # shellcheck disable=SC2128
 # esto carga las variables de entorno y verifica las versiones
-. "$(dirname "${BASH_SOURCE}")/../.load-env.sh"
+. "$(dirname "${BASH_SOURCE}")/.load-env.sh"
 #
 # Buscamos la lista de APIs para generar
 APIS=$("$BIN_DIR"/github/list-apis.sh)
 for API in $APIS ; do
-  # verificamos que el build sea correcto
+  # Borrando
+  rm -rf "$BASE_DIR"/packages/$API
+  #
   echo "-- Generando la API $API"
   export API
   pnpm run package-build
+  #
+  pnpm --dir packages/"$API" exec npe scripts.prepare "pnpm run build"
   pnpm --dir packages/"$API" install
 #  pnpm --dir packages/"$API" build
   echo "Done.."
