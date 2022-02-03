a5f95831f0cfea0944483f5608b64aa2b2e76135
mariano <mariano@benitez.nu>
2022-02-03 16:21:05 -0300

    - probando generar el indice... #7

--- bin/github/publish-apis.sh
+++ bin/github/publish-apis.sh
@@ -14,7 +14,7 @@ set -x # mostrar cada comando que se ejecuta
 # Publish Html
 echo "--> Publicando el HTML"
 ORIGIN=$(git config --get remote.origin.url)
-ORIGIN_FIXED="${ORIGIN/silohub/silohub-admin:$DEPLOY_TOKEN}"
+ORIGIN_FIXED="${ORIGIN/github.com/silohub-admin:$DEPLOY_TOKEN}@github.com"
 git remote set-url origin "$ORIGIN_FIXED"
 git remote -v
 pnpm exec gh-pages --dist "$BUILD_DIR/api-files" --branch gh-pages --dest "docs/$FCI_BRANCH" --message "APIs Updated - $BUILD_NUMBER" --user "Github Actions <it-admin@silohub.ag>"
