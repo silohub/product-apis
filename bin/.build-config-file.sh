88d662ec6f42536c647145086c37bbb1b61cb66e
mariano <mariano@benitez.nu>
2022-02-21 14:37:54 -0300

    swagger validator icon...

--- bin/.build-config-file.sh
+++ bin/.build-config-file.sh
@@ -15,11 +15,13 @@ API_VERSION=$(grep -e '^\s*version:.*' "$YAML_FILE" | cut -d \" -f 2 )
 API_TITLE=$(grep -e '^\s*title:.*' "$YAML_FILE" | cut -d \" -f 2 )
 API_TRANSLATED=${API//-/.}
 API_BASE_PATH=$(echo "${API//-/_}" | tr '[:lower:]' '[:upper:]')
+OPENAPI_URL="https://api.silohub.ag/docs/$FCI_BRANCH/$API/openapi.yaml"
 export API_VERSION
 export API_TRANSLATED
 export API_TITLE
 export API_BASE_PATH
-VARS="API API_TRANSLATED API_VERSION API_TITLE API_BASE_PATH $VARS_"
+export OPENAPI_URL
+VARS="API API_TRANSLATED API_VERSION API_TITLE API_BASE_PATH OPENAPI_URL $VARS_"
 #
 echo "{ " > "$TARGET"
 #
